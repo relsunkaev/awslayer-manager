1b9513eff65e15dfe2671885ab634f5e60d58af8
Ramazan Elsunkaev <ramazan@up-linq.com>
2022-01-21 16:38:44 -0700

    Version 1.0.14

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='awslayer-manager',
-    version='1.0.13',
+    version='1.0.14',
     packages=['awslayer'],
     install_requires=[
           'cursor',
